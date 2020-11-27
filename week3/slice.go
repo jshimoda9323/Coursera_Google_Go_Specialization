2e6792a9e05945747f87a3c36c1711e4a1a92a86
Jeff Shimoda <jshimoda@ca.ibm.com>
2020-11-26 21:07:40 -0500

    added slice.go

---
+++ week3/slice.go
@@ -0,0 +1,62 @@
+package main
+
+import "io"
+import "fmt"
+import "strconv"
+import "sort"
+
+func main() {
+    var intarray []int = make([]int, 0, 3)
+    var err error = nil
+    var input_string string
+    for ; err == nil; {
+        _, err = fmt.Scanf("%s",&input_string)
+        if err == io.EOF || input_string == "X" { break; }
+        var input_integer int
+        input_integer, err = strconv.Atoi(input_string)
+        if err != nil { break; }
+        intarray = append(intarray, input_integer)
+        sort.Ints(intarray)
+        fmt.Println(intarray)
+    }
+    return
+
+
+
+
+
+
+
+
+    //var myarray [5]int
+    //var mylitarray [5]int = [5]{1,2,3,4,5}
+    //var x := [...]int{1,2,3,4,5}
+
+    //for i, v range x {
+    //    fmt.Printf("%d, %d", i, v)
+    //}
+    //myslice = make([]int, 10)  // length=10, cap=10
+    //myslice2 = make([]int, 10, 15) // length=10, cap=15
+
+    //var idMap map[string]int
+    //idMap = make(map[string]int) { "joe" : 123 }
+    //fmt.Println(idMap["jod"])
+    //delete(idMap["joe"])
+    //id, p := idMap["joe"] // ,id is the value, p is a bool for existence
+    //for key, val := range idMap {
+    //    fmt.Println(key, val)
+    //}
+
+
+    //type struct Person {
+    //    name string
+    //    addr string
+    //    phone string
+    //}
+    //var p1 Person
+    //p1.name = "Joe"
+    //p1.addr = "666 Road to Oblivion"
+    //p2 := new(Person)
+    //p3 := Person(name: "joe", addr: "a st.", phone: "123")
+
+}
